1c405ce041425ca539158a17eacc87b5f190e97d
Lolo <loom4k.twitch@gmail.com>
2021-11-07 13:22:05 -0500

    ✨ Added label translations

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -14,43 +14,47 @@ module.exports = class extends Event {
         let guildData = await this.client.Database.fetchGuild(interaction.guild.id)
         let userData = await this.client.Database.fetchUser(interaction.member.id)
         let langData = require(`../data/languages/client/${userData.lang}.json`)
+        let labelLang = require(`../data/languages/labels/${userData.lang}.json`)
 
         let data = {}
         data.guild = guildData
         data.user = userData
         data.lang = langData
+        data.labels = labelLang
 
         const cmd = this.client.slashCommands.get(interaction.commandName)
-        try {
-            let cmdCooldown = cmd.cooldown || 5
-            const cooldown = cmdCooldown * 1000
-            if (cmdCooldown) {
-                if (this.client.Timeout.has(`${cmd.name}${interaction.user.id}`)) {
-                    let cooldownEmbed = new MessageEmbed()
-                        .setTitle(data.lang.on_cooldown)
-                        .setDescription(`${data.lang.ready_in.replace('{time}', ms(this.client.Timeout.get(cmd.name + interaction.user.id) - Date.now(), { long: true }))}`)
-                        .setColor(this.client.colors.redish)
-                    return interaction.reply({ embeds: [cooldownEmbed], ephemeral: true })
-                } else {
-                    this.client.Timeout.set(`${cmd.name}${interaction.user.id}`, Date.now() + cooldown)
-                    try {
-                        if (interaction.isCommand()) {
-                            await interaction.deferReply({ ephemeral: false }).catch(() => {})
+        if(interaction.isCommand()) {
+            try {
+                let cmdCooldown = cmd.cooldown || 5
+                const cooldown = cmdCooldown * 1000
+                if (cmdCooldown) {
+                    if (this.client.Timeout.has(`${cmd.name}${interaction.user.id}`)) {
+                        let cooldownEmbed = new MessageEmbed()
+                            .setTitle(data.lang.on_cooldown)
+                            .setDescription(`${data.lang.ready_in.replace('{time}', ms(this.client.Timeout.get(cmd.name + interaction.user.id) - Date.now(), { long: true }))}`)
+                            .setColor(this.client.colors.redish)
+                        return interaction.reply({ embeds: [cooldownEmbed], ephemeral: true })
+                    } else {
+                        this.client.Timeout.set(`${cmd.name}${interaction.user.id}`, Date.now() + cooldown)
+                        try {
+                            if (interaction.isCommand()) {
+                                await interaction.deferReply({ ephemeral: false }).catch(() => {})
+                            }
+                            cmd.run(this.client, interaction, data).catch(e => {})
+                        } catch (error) {
+                            logger.error(error, { label: 'Error' })
+                            return interaction.followUp({ content: data.lang.error, ephemeral: true })
                         }
-                        cmd.run(this.client, interaction, data).catch(e => {})
-                    } catch (error) {
-                        logger.error(error, { label: 'Error' })
-                        return interaction.followUp({ content: data.lang.error, ephemeral: true })
                     }
+    
+                    setTimeout(() => {
+                        this.client.Timeout.delete(`${cmd.name}${interaction.user.id}`)
+                    }, cooldown)
                 }
-
-                setTimeout(() => {
-                    this.client.Timeout.delete(`${cmd.name}${interaction.user.id}`)
-                }, cooldown)
+    
+            } catch (e) {
+                console.log(e)
             }
-
-        } catch (e) {
-            console.log(e)
         }
     }
 }
