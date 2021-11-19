d79a45100072a53889ec72f8ca41b8f9e9a3a4e0
Loïk Mallat <loom4k.twitch@gmail.com>
2021-11-19 10:36:11 -0500

    📝 Update README.md

--- test.js
+++
@@ -1,8 +0,0 @@
-const { splitBar, filledBar } = require('string-progressbar');
-
-const split = splitBar(100, 50);
-const filled = filledBar(100, 50);
-
-console.log("[" + split[0] + "] [" + split[1] + "%]\n\n");
-
-console.log(filled[0] + " [" + filled[1] + "%]");
