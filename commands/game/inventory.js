f12eae2f7dfc0c59fe3f939127efeb4fb96e6f23
null <60427892+vierofernando@users.noreply.github.com>
2021-11-22 21:46:26 +0700

    refactor(inventory): use Array.prototype.map

--- commands/game/inventory.js
+++ commands/game/inventory.js
@@ -5,17 +5,9 @@ module.exports = {
     description: "Shows your inventory",
     run: async(client, interaction, data) => {
         try {
-            let reply = ``
-
-            for (const item of data.user.inventory) {
-                const { name, amount, emoji } = item
-
-                reply += `${amount} ${name}(s)\n`
-            }
-
             const embed = new MessageEmbed()
                 .setAuthor(data.lang.inventory_title.replace('{user}', interaction.user.tag), interaction.user.displayAvatarURL({ dynamic: true }))
-                .setDescription(reply)
+                .setDescription(data.user.inventory.map(({ name, amount }) => `${amount} ${name}(s)`).join('\n'))
                 .setColor(client.colors.greeny)
 
             interaction.followUp({ embeds: [embed] })
