dade8bc09a2ab7ac07ddf2f4a07530989d306c88
null <60427892+vierofernando@users.noreply.github.com>
2021-11-22 22:04:35 +0700

    refactor(shop): refactor map callback

--- commands/game/shop.js
+++ commands/game/shop.js
@@ -29,34 +29,21 @@ module.exports = {
      * @param {String[]} args
      */
     run: async(client, interaction, data) => {
-        const storeItm = storeItems.map((value, index) => {
-            return [
-                `**${value.price.toLocaleString()} ${client.emoji.cod}** ― ${value.emoji || ''} **${value.name}** (${value.id})`,
-            ].join('\n')
-        }).join('\n\n')
-
-        const marketItm = marketItems.map((value, index) => {
-            return [
-                `**${value.price.toLocaleString()} ${client.emoji.cod}** ― **${value.name}** (${value.id})`,
-            ].join('\n')
-        }).join('\n\n')
+        const storeItm = storeItems.map(({ price, emoji, name, id }) => `**${price.toLocaleString()} ${client.emoji.cod}** ― ${emoji || ''} **${name}** (${id})`).join('\n\n')
+        const marketItm = marketItems.map(({ price, name, id }) => `**${price.toLocaleString()} ${client.emoji.cod}** ― **${name}** (${id})`).join('\n\n')
 
         const storeEmbed = new MessageEmbed()
             .setTitle(data.lang.store.convenience_title + ' ' + client.emoji.cod)
             .setDescription('*' + data.lang.store.item_buy_indications + '*\n\n' + storeItm)
             .setAuthor(client.user.tag, client.user.displayAvatarURL())
-            .setColor(client.colors.greeny)
+            .setColor(client.colors.greeny);
 
         const marketEmbed = new MessageEmbed()
             .setTitle(data.lang.store.market_title + ' ' + client.emoji.sushi)
             .setDescription('*' + data.lang.store.item_buy_indications + '*\n\n' + marketItm)
             .setAuthor(client.user.tag, client.user.displayAvatarURL())
-            .setColor(client.colors.greeny)
+            .setColor(client.colors.greeny);
 
-        if (interaction.options.getString('shop') == 'market') {
-            interaction.followUp({ embeds: [marketEmbed] })
-        } else {
-            interaction.followUp({ embeds: [storeEmbed] })
-        }
+        interaction.followUp({ embeds: [interaction.options.getString('shop') === 'market' ? marketEmbed : storeEmbed] });
     },
 };
